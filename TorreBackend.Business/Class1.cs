a8dc0e80419ed2b73b5837105ce40c57ba0e537a
juancriveros <juan.camilo.riveros@hotmail.com>
2021-01-13 12:03:30 -0500

    Agregar archivos de proyecto.

---
+++ TorreBackend.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TorreBackend.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ TorreBackend.Entities/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TorreBackend.Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
